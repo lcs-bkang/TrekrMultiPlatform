aa9034748e98c6598c32817a55c99befa5474aa6
Bradley Kang <bkang@lcs.on.ca>
2022-01-18 09:45:54 -0500

    Laid out detail view and added view modifiers to it

--- Shared/Views/LocationDetail.swift
+++ Shared/Views/LocationDetail.swift
@@ -18,6 +18,36 @@ struct LocationDetail: View {
             Image(location.heroPicture)
                 .resizable()
                 .scaledToFit()
+            
+            HStack {
+                
+                Text(location.country)
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom)
+                
+                Spacer()
+
+            }
+            .padding(.horizontal)
+            
+            Text(location.description)
+                .padding(.horizontal)
+            
+            HStack {
+                
+                Text("Did you know?")
+                    .font(.title3)
+                    .bold()
+                    .padding(.vertical)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text(location.more)
+                .padding([.horizontal, .bottom])
         }
         .navigationTitle(location.name)
     }
